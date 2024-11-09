a36ee201a9509f442de876c741f22365e9556a15
zaarinas <nassim.zaari@etu.utc.fr>
2024-11-09 14:35:47 +0100

    Repository pattern and Dto added

---
+++ src/main/java/com/rungroup/web/repository/ClubRepository.java
@@ -0,0 +1,7 @@
+package com.rungroup.web.repository;
+import com.rungroup.web.models.Club;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClubRepository extends JpaRepository<Club,Long>{
+    
+}
