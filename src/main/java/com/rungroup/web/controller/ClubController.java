beeb867e4af4a90fcd202c5e3172ef8442f5fcd4
zaarinas <nassim.zaari@etu.utc.fr>
2024-11-10 13:29:56 +0100

    controller added

---
+++ src/main/java/com/rungroup/web/controller/ClubController.java
@@ -0,0 +1,30 @@
+package com.rungroup.web.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import com.rungroup.web.dto.ClubDto;
+import com.rungroup.web.service.ClubService;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class ClubController {
+    private ClubService clubService;
+
+    @Autowired
+    public ClubController(ClubService clubService){
+        this.clubService = clubService;
+    }
+
+    @GetMapping("/clubs")
+    public String ListClubs(Model model) {
+        List<ClubDto> clubs = clubService.findAllClubs();
+        model.addAttribute("clubs",clubs);
+        return "clubs-list";
+    }
+    
+}
--- src/main/java/com/rungroup/web/service/impl/ClubServiceImpl.java
+++ src/main/java/com/rungroup/web/service/impl/ClubServiceImpl.java
@@ -3,16 +3,16 @@ package com.rungroup.web.service.impl;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.rungroup.web.dto.ClubDto;
 import com.rungroup.web.models.Club;
 import com.rungroup.web.repository.ClubRepository;
 import com.rungroup.web.service.ClubService;
 
+@Service
 public class ClubServiceImpl implements ClubService{
     private ClubRepository clubRepository;
-    @Autowired
     public ClubServiceImpl(ClubRepository ClubRepository) {
         this.clubRepository = clubRepository;
     }
